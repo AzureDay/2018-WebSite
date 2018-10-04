969d4ab9f7ba0c7498b80e2bad61259d61f81f04
Sergii Zachepylo <seza@ciklum.com>
2018-10-04 15:38:09 +0300

    Revert "Shedule page updated/Controller/Resources"
    This reverts commit 56fbf2e6475e4e4fbe31a107b5e07ea2bd06f7ac.

--- AzureDay.WebApp/AzureDay.WebApp.Database/Services/TimetableService.cs
+++ AzureDay.WebApp/AzureDay.WebApp.Database/Services/TimetableService.cs
@@ -3,28 +3,11 @@ using AzureDay.WebApp.Database.Entities;
 
 namespace AzureDay.WebApp.Database.Services
 {
-    public class TimetableService : BaseService<TimetableEntity, int>
+    public class TimetableService: BaseService<TimetableEntity, int>
     {
-        private readonly List<TimetableEntity> _timetables = new List<TimetableEntity>();
-
-        private readonly RoomService _roomService = new RoomService();
-        private readonly TopicService _topicService = new TopicService();
-
         protected override List<TimetableEntity> PopulateStorage()
         {
-            _timetables.AddRange(new List<TimetableEntity>
-            {
-                new TimetableEntity {Id = 1, TimeStart = "8:15", TimeEnd = "9:00", Room = _roomService.CoffeeBreak, Topic = _topicService.CoffeeBreak },
-                new TimetableEntity { TimeStart = "10:15", TimeEnd = "10:30", Room = _roomService.CoffeeBreak},
-                new TimetableEntity { TimeStart = "11:30", TimeEnd = "12:00", Room = _roomService.CoffeeBreak, Topic = _topicService.CoffeeBreak },
-                new TimetableEntity { TimeStart = "13:00", TimeEnd = "13:15", Room = _roomService.CoffeeBreak },
-                new TimetableEntity { TimeStart = "14:15", TimeEnd = "14:45", Room = _roomService.CoffeeBreak, Topic = _topicService.Lunch },
-                new TimetableEntity { TimeStart = "15:45", TimeEnd = "16:00", Room = _roomService.CoffeeBreak },
-                new TimetableEntity { TimeStart = "17:00", TimeEnd = "17:30", Room = _roomService.CoffeeBreak, Topic = _topicService.CoffeeBreak },
-                new TimetableEntity { TimeStart = "19:15", TimeEnd = "21:00", Room = _roomService.CoffeeBreak, Topic = _topicService.Afterparty }
-            });
-
-            return _timetables;
+            throw new System.NotImplementedException();
         }
     }
 }
--- AzureDay.WebApp/AzureDay.WebApp.WWW/Controllers/HomeController.cs
+++ AzureDay.WebApp/AzureDay.WebApp.WWW/Controllers/HomeController.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AzureDay.WebApp.WWW.Models;
 using AzureDay.WebApp.WWW.Models.Home;
-using AzureDay.WebApp.Database.Services;
-using AzureDay.WebApp.Database.Enums;
+using Microsoft.AspNetCore.Rewrite.Internal;
 
 namespace AzureDay.WebApp.WWW.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly Lazy<RoomService> _roomService = new Lazy<RoomService>(() => new RoomService());
-        private readonly Lazy<TimetableService> _timetableService = new Lazy<TimetableService>(() => new TimetableService());
-
         public async Task<IActionResult> Index()
         {
             var model = new IndexModel();
@@ -24,21 +18,7 @@ namespace AzureDay.WebApp.WWW.Controllers
 
         public async Task<IActionResult> Schedule()
         {
-            var model = new ScheduleModel();
-
-            model.Rooms = _roomService.Value
-                .GetAll()
-                .Where(x => x.RoomType == RoomType.LectureRoom)
-                .ToList();
-
-            model.Timetables = _timetableService.Value
-                .GetAll()
-                .GroupBy(
-                    t => t.TimeStart,
-                    (key, timetables) => timetables.OrderBy(t => t.Room.ColorNumber).ToList())
-                .ToList();
-
-            return View(model);
+            return View();
         }
         
         public async Task<IActionResult> Workshops()
